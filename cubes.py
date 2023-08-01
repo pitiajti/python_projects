11ddebd4deb3041d255a15c865bc08c6a6e3f8b2
pitiajti <pitiajti@gmail.com>
2023-08-01 14:14:05 +0200

    modifying cubes.py and adding random_walk.py

--- cubes.py
+++ cubes.py
@@ -12,9 +12,10 @@ ax.plot(input_values, squares, linewidth=3)
 ax.set_title("Square numbers", fontsize=24)
 ax.set_xlabel("Value", fontsize=14)
 ax.set_ylabel("Square of Value", fontsize=14)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 # Set size of tick labels.
 ax.tick_params(labelsize=14)
 
-# plt.show()
-plt.savefig('cubes_plot.png', bbox_inches='tight')
+plt.show()
+# plt.savefig('cubes_plot.png', bbox_inches='tight')
